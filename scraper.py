f331bac045ee896b06e558264161a6f4127019f0
Melanie <melanie.basso@melscodingcave.com>
2025-10-01 12:17:41 -0400

    expand to multiple pages

--- scraper.py
+++ scraper.py
@@ -1,42 +1,52 @@
 import requests
 from bs4 import BeautifulSoup
 import pprint
+import time
 
-res = requests.get('https://news.ycombinator.com/news')
-# print(res.text)
-
-soup = BeautifulSoup(res.text, 'html.parser')
-# print(soup)
-# print(soup.body)
-# print(soup.body.content)
-# print(soup.find_all('div'))
-# print(soup.find_all('a'))
-# print(soup.find(id='score_20514755))
-
-# need to use .titleline instead of .storylink and .titleline > a is used because a tag is under titleline element
-
-# print(soup.select('a'))
-# print(soup.select('div))
-# print(soup.select(.score))
-# print(soup.select(#score_20514755))
-links = soup.select('.titleline')
-subtext = soup.select('.subtext')
-# print(votes[0].get('id'))
+base_url = 'https://news.ycombinator.com/news'
 
 def sort_stories_by_votes(hnlist):
-    return sorted(hnlist, key= lambda k:k['votes'], reverse=True)
+            return sorted(hnlist, key= lambda k:k['votes'], reverse=True)
 
-def create_custom_hn(links, subtext):
-    hn = []
-    for idx, item in enumerate(links):
+def create_custom_hn(links, subtext, min_votes=100):
+            hn = []
+            for idx, item in enumerate(links):
+                title = item.getText()
+                href = item.get('href', None)
+                vote_tag = subtext[idx].select_one('.score')
+                if vote_tag:
+                    points = int(vote_tag.getText().replace(' points', ''))
+                    if points >= min_votes:
+                        hn.append({'title': title, 'link': href, 'votes': points})
+            return hn
+
+def scrape_hn(min_votes=100, target_count=20, delay=1):
+    current_page = 1
+    all_stories = []
+
+    with requests.Session() as session:
+        while len(all_stories) < target_count:
+            url = f'{base_url}?p={current_page}'
+            res = session.get(url)
+            soup = BeautifulSoup(res.text, 'html.parser')
+
+            links = soup.select('.titleline')
+            subtext = soup.select('.subtext')
+            
+            new_stories = create_custom_hn(links, subtext, min_votes)
+            all_stories.extend(new_stories)
+
+            more_link = soup.find('a', id='morelink')
+            
+            if not more_link:
+                break
+            
+            current_page += 1
+            time.sleep(delay)
+            
+    return sort_stories_by_votes(all_stories)[:target_count]
+
+if __name__ == '__main__':
+    stories = scrape_hn(min_votes=100, target_count=20, delay=2)
+    pprint.pprint(stories)
         
-        title = item.getText()
-        href = item.get('href', None)
-        vote = subtext[idx].select('.score')
-        if len(vote):
-            points = int(vote[0].getText().replace(' points', ''))
-            if points > 99:
-               hn.append({'title': title, 'link': href, 'votes': points})
-    return sort_stories_by_votes(hn)
-
-pprint.pprint(create_custom_hn(links, subtext))
