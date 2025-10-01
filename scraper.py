4c0d1259cd34a15a0a82e36bca517fda99db1a05
Melanie <melanie.basso@melscodingcave.com>
2025-10-01 11:34:38 -0400

    Update to allow sort of first page returns > 100 votes

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import pprint
 
 res = requests.get('https://news.ycombinator.com/news')
 # print(res.text)
@@ -19,17 +20,23 @@ soup = BeautifulSoup(res.text, 'html.parser')
 # print(soup.select(.score))
 # print(soup.select(#score_20514755))
 links = soup.select('.titleline')
-votes = soup.select('.score')
+subtext = soup.select('.subtext')
 # print(votes[0].get('id'))
 
-def create_custom_hn(links, votes):
+def sort_stories_by_votes(hnlist):
+    return sorted(hnlist, key= lambda k:k['votes'], reverse=True)
+
+def create_custom_hn(links, subtext):
     hn = []
     for idx, item in enumerate(links):
         
-        title = links[idx].getText()
-        href = links[idx].get('href', None)
-        points = int(votes[idx].getText().replace(' points', ''))
-        hn.append({'title': title, 'link': href})
-    return hn
+        title = item.getText()
+        href = item.get('href', None)
+        vote = subtext[idx].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', ''))
+            if points > 99:
+               hn.append({'title': title, 'link': href, 'votes': points})
+    return sort_stories_by_votes(hn)
 
-create_custom_hn(links, votes)
+pprint.pprint(create_custom_hn(links, subtext))
